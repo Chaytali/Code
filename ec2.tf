38958019489e98ba8329a891a9ace51399653dc7
Chaytali <chaytali123@gmail.com>
2023-01-21 23:21:14 +0530

    Modify

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,8 @@ provider "aws" {
 
 resource "aws_instance" "rancherHost" {
     tags ={
-        name = "tf-rancher-host-rmf44"
+        name = "tf-rancher-host-rmf44",
+        env = "test"
     }
     ami                    = "ami-00bb465b8b23e8c9a"
     instance_type          = "t2.micro"
