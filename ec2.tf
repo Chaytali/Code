61a0eb31fe6d8ae00a033ab489db9a33d57bbd40
Chaytali <chaytali123@gmail.com>
2023-01-21 23:39:10 +0530

    Modify2

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-    access_key = "AKIAVCLKKHHMA5TPBDUW"
-    secret_key = "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"
+    access_key = "AKIAWFN6DT7CH67JSW4T"
+    secret_key = "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"
     region = "ap-south-1"
 }
 
-resource "aws_instance" "rancherHost" {
+resource "aws_instance" "Terraform" {
     tags ={
-        name = "tf-rancher-host-rmf44",
+        name = "terraform-test",
         env = "test"
     }
-    ami                    = "ami-00bb465b8b23e8c9a"
+    ami                    = "ami-0cca134ec43cf708f"
     instance_type          = "t2.micro"
-    vpc_security_group_ids =["sg-0674aa1c2f6559fdc"]
+    vpc_security_group_ids =["sg-0f948bc06d7a465e3"]
 }
